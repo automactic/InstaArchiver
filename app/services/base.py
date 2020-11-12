f0b8ae0a944ccb9901885b92902ffd4cf367ce13
ChrisLi <8294252+automactic@users.noreply.github.com>
2020-11-12 12:04:01 -0500

    aiofile & atime mtime (#7)
    * add aiofile
    
    * mtime atime
    
    * glue

--- app/services/base.py
+++ app/services/base.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from datetime import datetime
 from pathlib import Path
 
 import instaloader
@@ -19,7 +20,7 @@ class BaseService:
         except ValueError:
             self.group_id = None
 
-    def _change_file_ownership(self, path):
+    def _change_file_ownership(self, path: Path):
         """Change ownership of the directory or file to a specific user id or group id.
 
         :param path: the directory or file path to change the ownership
@@ -27,3 +28,14 @@ class BaseService:
 
         if self.user_id or self.group_id:
             shutil.chown(path, self.user_id, self.group_id)
+
+    @staticmethod
+    def _set_file_access_modify_time(path: Path, creation_time: datetime):
+        """Set file access time and modify time to post creation time.
+
+        :param path: the file path to change access time and modify time
+        :param creation_time: post creation time
+        """
+
+        timestamp = creation_time.timestamp()
+        os.utime(path, (timestamp, timestamp))
