5a76451c31eb893d4a496fec092aa3d2e1070543
ChrisLi <chris@kiwix.org>
2021-02-17 22:52:25 -0500

    api rppt

--- web/src/app/api.service.ts
+++ web/src/app/api.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from '../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class APIService {
   constructor(private httpClient: HttpClient) { }
-  // root = 'http://localhost:37500'
-  root = ''
 
   savePost(shortcode: string) {
-    let url = this.root + '/api/posts/from_url/'
+    let url = environment.apiRoot + '/api/posts/from_url/'
     let payload = {'shortcode': shortcode}
     this.httpClient.post(url, payload).subscribe(data => {
       console.log(data);
