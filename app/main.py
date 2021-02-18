fd38ba0d31604846f562624e69bca6d3e5fabfc3
ChrisLi <chris@kiwix.org>
2021-02-17 23:06:05 -0500

    API request

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from api.requests import PostCreationFromShortcode
+
 app = FastAPI()
 app.mount('/web', StaticFiles(directory='/web', html=True), name='web')
 
+
 @app.post('/api/posts/from_url/')
-def create_post_from_url():
-    return {'Hello': 'World'}
+def create_post_from_url(request: PostCreationFromShortcode):
+    return {'Hello': request.shortcode}
