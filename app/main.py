3b3fe8a2da5d4a194215b9bc90146ecb9b6e27f3
ChrisLi <chris@kiwix.org>
2021-02-18 08:33:08 -0500

    web redirections

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
 from api.requests import PostCreationFromShortcode
 
 app = FastAPI()
 app.mount('/web', StaticFiles(directory='/web', html=True), name='web')
+
+
+@app.get('/')
+def root():
+    return RedirectResponse(url='web/index.html')
+
+
+@app.get('/index.html')
+def index():
+    return RedirectResponse(url='web/index.html')
 
 
 @app.post('/api/posts/from_url/')
