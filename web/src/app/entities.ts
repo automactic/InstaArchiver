cea26f93fdf56372286b45cc34016cdb919d04b5
ChrisLi <chris@kiwix.org>
2021-02-26 20:11:24 -0500

    profile posts

--- web/src/app/entities.ts
+++ web/src/app/entities.ts
@@ -1,10 +1,18 @@
 import { Optional } from "@angular/core";
 
+export interface PostsSummary {
+  count: number
+	earliest_time?: Date
+	latest_time?: Date
+}
+
 export interface Profile {
-    username: string
-    full_name: string
-    biography: string
-    auto_update: boolean
-    last_update?: Date
-    image_filename: string
+	username: string
+	full_name: string
+	display_name: string
+	biography: string
+	auto_update: boolean
+	last_update?: Date
+	image_filename: string
+	posts?: PostsSummary
 }
