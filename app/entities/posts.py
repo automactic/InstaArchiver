49ef42dfed897aba76f9abaacee808c21782cbe0
ChrisLi <chris@kiwix.org>
2021-02-28 11:11:32 -0500

    PostListResult

--- app/entities/posts.py
+++ app/entities/posts.py
@@ -1,6 +1,31 @@
 from datetime import datetime
+from enum import Enum
+from typing import List, Optional
 
 from pydantic import BaseModel
+
+
+class PostType(Enum):
+    IMAGE = 'image'
+    VIDEO = 'video'
+    SIDECAR = 'sidecar'
+
+
+class Post(BaseModel):
+    shortcode: str
+    owner_username: str
+    creation_time: datetime
+    type: PostType
+    caption: Optional[str]
+    caption_hashtags: [str]
+    caption_mentions: [str]
+
+
+class PostListResult(BaseModel):
+    posts: List[Post]
+    limit: int
+    offset: int
+    count: int
 
 
 class PostCreationFromShortcode(BaseModel):
