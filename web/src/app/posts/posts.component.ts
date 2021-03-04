f742d8f046cd318894f674dffef06d5d93bd961f
ChrisLi <chris@kiwix.org>
2021-03-03 22:42:21 -0500

    navigation

--- web/src/app/posts/posts.component.ts
+++ web/src/app/posts/posts.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Optional } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 import { Post, PostItem, PostService } from '../services/post.service';
 import { ProfileService } from '../services/profile.service';
@@ -14,11 +17,17 @@ export class PostsComponent implements OnInit {
   profileService: ProfileService;
 
   loading = false;
+  username$: Observable<string>;
   posts: Post[] = [];
   
-  constructor(postService: PostService, profileService: ProfileService) {
+  constructor(private route: ActivatedRoute, private router: Router, postService: PostService, profileService: ProfileService) {
     this.postService = postService;
     this.profileService = profileService;
+    this.username$ = this.route.queryParamMap.pipe(
+      switchMap(queryParam => {
+        return queryParam.get("username") ?? ""
+      })
+    );
   }
 
   ngOnInit(): void {
@@ -32,6 +41,10 @@ export class PostsComponent implements OnInit {
       this.posts.push(...response_data.posts);
       this.loading = false;
     })
+  }
+
+  selectedProfileChanged(profile_username: string) {
+    this.router.navigate(['/posts'], { queryParams: { profile: profile_username} })
   }
 
   delete(post: Post, item: PostItem, postIndex: number, itemIndex: number) {
