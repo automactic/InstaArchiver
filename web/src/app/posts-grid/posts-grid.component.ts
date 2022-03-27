fa2dbf5fb690c2de5bc3cd254c45b56822268ad0
Chris Li <chris@kiwix.org>
2022-03-27 18:24:38 -0400

    grid setup

--- web/src/app/posts-grid/posts-grid.component.ts
+++ web/src/app/posts-grid/posts-grid.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import { PostService, Post } from '../services/post.service';
 
 @Component({
   selector: 'posts-grid',
@@ -6,8 +10,28 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./posts-grid.component.scss']
 })
 export class PostsGridComponent {
-  @Input() username?: string;
+  postService: PostService;
+  posts: Post[] = [];
 
-  constructor() { }
+  username?: string;
+  year?: string;
+  month?: string;
 
+  constructor(
+    private route: ActivatedRoute,
+    postService: PostService,
+  ) {
+    this.postService = postService
+    this.route.queryParamMap.pipe(
+      switchMap(queryParams => {
+        this.username = queryParams.get('username') ?? undefined;
+        this.year = queryParams.get('year') ?? undefined;
+        this.month = queryParams.get('month') ?? undefined;
+        return this.postService.list(0, 25, this.username, this.year, this.month);
+      })
+    ).subscribe(response => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      this.posts = response.posts;
+    })
+  }
 }
--- web/src/app/posts/posts.component.ts
+++ web/src/app/posts/posts.component.ts
@@ -21,24 +21,12 @@ export class PostsComponent {
   month?: string;
 
   constructor(
-    private route: ActivatedRoute,  
-    postService: PostService, 
+    private route: ActivatedRoute,
+    postService: PostService,
     profileService: ProfileService
   ) {
     this.postService = postService
     this.profileService = profileService
-    this.route.queryParamMap.pipe(
-      switchMap(queryParams => {
-        this.username = queryParams.get('username') ?? undefined;
-        this.year = queryParams.get('year') ?? undefined;
-        this.month = queryParams.get('month') ?? undefined;
-        return this.postService.list(0, 5, this.username, this.year, this.month);
-      })
-    ).subscribe(response => {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-      // this.loading = false;
-      // this.posts = response.posts;
-    })
     this.profile$ = this.route.queryParamMap.pipe(
       switchMap(queryParams => {
         return this.profileService.get(queryParams.get('username') ?? '')
