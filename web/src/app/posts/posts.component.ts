05572816888f4eb56f6c7d421573fbd8c829309a
Chris Li <chris@kiwix.org>
2022-03-29 16:13:10 -0400

    post

--- web/src/app/posts/posts.component.ts
+++ web/src/app/posts/posts.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
+import { NbWindowService, NbWindowControlButtonsConfig } from '@nebular/theme';
 
 import { ProfileService, Profile } from '../services/profile.service';
-import { PostService, Post } from '../services/post.service';
+import { ProfileEditComponent } from '../profile-edit/profile-edit.component';
 
 @Component({
   selector: 'app-posts',
@@ -12,21 +13,20 @@ import { PostService, Post } from '../services/post.service';
   styleUrls: ['./posts.component.scss']
 })
 export class PostsComponent {
-  postService: PostService;
   profileService: ProfileService
 
   profile$: Observable<Profile>
-  username?: string;
-  year?: string;
-  month?: string;
+  username?: string
+  year?: string
+  month?: string
+  selectedShortcode?: string
 
   constructor(
     private route: ActivatedRoute,
     private router: Router, 
-    postService: PostService,
+    private windowService: NbWindowService,
     profileService: ProfileService
   ) {
-    this.postService = postService
     this.profileService = profileService
     this.profile$ = this.route.queryParamMap.pipe(
       switchMap(queryParams => {
@@ -37,6 +37,7 @@ export class PostsComponent {
       this.username = queryParams.get('username') ?? undefined
       this.year = queryParams.get('year') ?? undefined
       this.month = queryParams.get('month') ?? undefined
+      this.selectedShortcode = queryParams.get('selected') ?? undefined
     })
   }
 
@@ -58,5 +59,23 @@ export class PostsComponent {
       queryParams: { month: month }, 
       queryParamsHandling: 'merge' 
     });
+  }
+
+  openEditWindow(username: string, display_name: string) {
+    const config: NbWindowControlButtonsConfig = {
+      minimize: false,
+      maximize: false,
+      fullScreen: false,
+      close: true,
+    }
+    const context = { username: username, display_name: display_name }
+    this.windowService.open(
+      ProfileEditComponent, 
+      { title: `Edit Profile: ${username}`, buttons: config, context: context }
+    )
+  }
+
+  openInstagramProfile(username: string) {
+    window.open(`https://www.instagram.com/${username}/`, '_blank')
   }
 }
