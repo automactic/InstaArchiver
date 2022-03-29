b7143f69e125b635fa3a72cf9f814f0f2f081065
Chris Li <chris@kiwix.org>
2022-03-29 16:09:56 -0400

    profile detail

--- web/src/app/profile-detail/profile-detail.component.ts
+++ web/src/app/profile-detail/profile-detail.component.ts
@@ -1,8 +1,6 @@
 import { Component, Input } from '@angular/core';
-import { NbWindowService, NbWindowControlButtonsConfig } from '@nebular/theme';
 
-import { ProfileService, Profile } from '../services/profile.service';
-import { ProfileEditComponent } from '../profile-edit/profile-edit.component';
+import { Profile } from '../services/profile.service';
 
 @Component({
   selector: 'profile-detail',
@@ -10,31 +8,7 @@ import { ProfileEditComponent } from '../profile-edit/profile-edit.component';
   styleUrls: ['./profile-detail.component.scss']
 })
 export class ProfileDetailComponent {
-  @Input() profile: Profile | null = null
-  profileService: ProfileService
+  @Input() profile?: Profile
 
-  constructor(
-    profileService: ProfileService,
-    private windowService: NbWindowService,
-  ) {
-    this.profileService = profileService
-  }
-
-  openEditWindow(username: string, display_name: string) {
-    const config: NbWindowControlButtonsConfig = {
-      minimize: false,
-      maximize: false,
-      fullScreen: false,
-      close: true,
-    }
-    const context = { username: username, display_name: display_name }
-    this.windowService.open(
-      ProfileEditComponent, 
-      { title: `Edit Profile: ${username}`, buttons: config, context: context }
-    )
-  }
-
-  openInstagramProfile(username: string) {
-    window.open(`https://www.instagram.com/${username}/`, '_blank')
-  }
+  constructor() { }
 }
