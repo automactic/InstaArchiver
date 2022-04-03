b49712d89de4cf4c2a7d482655249c22548adcff
Chris Li <chris@kiwix.org>
2022-04-03 15:22:31 -0400

    post grid

--- web/src/app/posts-grid/posts-grid.component.ts
+++ web/src/app/posts-grid/posts-grid.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { filter, switchMap } from 'rxjs/operators';
 
-import { PostService, Post } from '../services/post.service';
+import { PostService } from '../services/post.service';
 
 @Component({
   selector: 'posts-grid',
@@ -11,7 +11,6 @@ import { PostService, Post } from '../services/post.service';
 })
 export class PostsGridComponent {
   postService: PostService
-  posts: Post[] = []
 
   username?: string
   year?: string
@@ -36,9 +35,6 @@ export class PostsGridComponent {
         this.month = queryParams.get('month') ?? undefined
         return this.postService.list(0, 96, this.username, this.year, this.month)
       })
-    ).subscribe(response => {
-      window.scrollTo({ top: 0, behavior: 'smooth' })
-      this.posts = response.posts
-    })
+    ).subscribe()
   }
 }
