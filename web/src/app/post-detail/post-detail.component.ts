f902c7e81c3cffab9a2efc0d97d9d4022fdda752
Chris Li <chris@kiwix.org>
2022-04-03 11:04:29 -0400

    delete post

--- web/src/app/post-detail/post-detail.component.ts
+++ web/src/app/post-detail/post-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
-import { PostService, Post } from '../services/post.service';
+import { PostService, Post, PostItem } from '../services/post.service';
 
 @Component({
   selector: 'post-detail',
@@ -18,5 +18,15 @@ export class PostDetailComponent implements OnChanges {
 
   ngOnChanges(changes: SimpleChanges) {
     this.post = this.postService.get(changes['shortcode'].currentValue)
+  }
+
+  delete(post: Post, item: PostItem) {
+    this.postService.delete(post.shortcode, item.index).subscribe( _ => {
+      // if (post.items.length == 1) {
+      //   this.posts.splice(postIndex, 1);
+      // } else {
+      //   post.items.splice(itemIndex, 1);
+      // }
+    })
   }
 }
