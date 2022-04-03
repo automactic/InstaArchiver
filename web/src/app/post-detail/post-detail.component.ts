b0b70ac16f390c838a1c88db95abe9b3e0723fb7
Chris Li <chris@kiwix.org>
2022-04-03 10:38:23 -0400

    post caption

--- web/src/app/post-detail/post-detail.component.ts
+++ web/src/app/post-detail/post-detail.component.ts
@@ -1,17 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
-import { PostService } from '../services/post.service';
+import { PostService, Post } from '../services/post.service';
 
 @Component({
   selector: 'post-detail',
   templateUrl: './post-detail.component.html',
   styleUrls: ['./post-detail.component.scss']
 })
-export class PostDetailComponent {
+export class PostDetailComponent implements OnChanges {
   @Input() shortcode?: string
+  post?: Post
   postService: PostService
 
   constructor(postService: PostService) {
     this.postService = postService
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.post = this.postService.get(changes['shortcode'].currentValue)
   }
 }
