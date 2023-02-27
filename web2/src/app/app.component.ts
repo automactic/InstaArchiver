ec4215dbafb35aded39731ea8ae2bcc28e8d9698
Chris Li <chris@kiwix.org>
2023-02-27 06:59:03 -0500

    selector

--- web2/src/app/app.component.ts
+++ web2/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { ActivatedRoute, Router } from '@angular/router';
 import { NbSidebarService, NbSidebarState, NbThemeService } from '@nebular/theme';
 
 import { ProfileService, ListProfilesResponse } from './services/profile.service';
@@ -12,13 +12,18 @@ import { ProfileService, ListProfilesResponse } from './services/profile.service
 })
 export class AppComponent {
   title = 'InstaArchiver'
+  sidebarState: NbSidebarState = 'expanded'
+  year?: string
+  month?: string
+
   response$?: Observable<ListProfilesResponse>
   profileService: ProfileService
-  sidebarState: NbSidebarState = 'expanded'
 
   constructor(
     private sidebarService: NbSidebarService, 
     private themeService: NbThemeService, 
+    private route: ActivatedRoute,
+    private router: Router, 
     profileService: ProfileService
   ) {
     window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', e => {
@@ -27,11 +32,35 @@ export class AppComponent {
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
       e.matches && this.themeService.changeTheme('dark')
     })
+    this.route.queryParamMap.subscribe(queryParams => {
+      this.year = queryParams.get('year') ?? undefined
+      this.month = queryParams.get('month') ?? undefined
+    })
     this.response$ = profileService.list()
     this.profileService = profileService
   }
 
   toggle() {
     this.sidebarService.toggle(true, 'left')
+  }
+
+  selectedYearChanged(year: string) {
+    var queryParams: Record<string, string | null> = { year: year };
+    if (year == null) {
+      queryParams['month'] = null;
+    }
+    this.router.navigate([], { 
+      relativeTo: this.route, 
+      queryParams: queryParams, 
+      queryParamsHandling: 'merge' 
+    });
+  }
+
+  selectedMonthChanged(month: string) {
+    this.router.navigate([], { 
+      relativeTo: this.route, 
+      queryParams: { month: month }, 
+      queryParamsHandling: 'merge' 
+    });
   }
 }
