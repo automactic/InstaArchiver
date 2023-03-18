a0d04a1ca5f96317fd9ed97f444ff96773002d52
Chris Li <chris@kiwix.org>
2023-03-18 17:30:04 -0400

    inf scroll

--- web/src/app/posts-grid/posts-grid.component.ts
+++ web/src/app/posts-grid/posts-grid.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, BehaviorSubject } from 'rxjs';
-import { filter, combineLatestWith, switchMap, tap, map } from 'rxjs/operators';
+import { filter, combineLatestWith, switchMap, map } from 'rxjs/operators';
 
-import { PostService, ListPostsResponse, Post } from '../services/post.service';
-import { Profile, ProfileService } from '../services/profile.service';
+import { PostService, Post } from '../services/post.service';
 
 
 @Component({
@@ -13,17 +12,18 @@ import { Profile, ProfileService } from '../services/profile.service';
   styleUrls: ['./posts-grid.component.scss']
 })
 export class PostsGridComponent {
+  postService: PostService;
+  
   username?: string
   year?: string
   month?: string
-  response$: Observable<ListPostsResponse>
+  posts = new Map<String, Post>()
   selectedPost$: Observable<Post | null>
   selectedUsername$: Observable<string | null>
-  postService: PostService;
 
-  constructor(private route: ActivatedRoute, postService: PostService, profileService: ProfileService) {
+  constructor(private route: ActivatedRoute, postService: PostService) {
     this.postService = postService
-    this.response$ = this.route.paramMap.pipe(
+    this.route.paramMap.pipe(
       combineLatestWith(this.route.queryParamMap),
       filter(([params, queryParams]) => {
         return (
@@ -33,25 +33,21 @@ export class PostsGridComponent {
           queryParams.get('month') != this.month
         )
       }),
-      tap(([params, queryParams]) => {
-        this.username = params.get('username') ?? undefined
-        this.year = queryParams.get('year') ?? undefined
-        this.month = queryParams.get('month') ?? undefined
-      }),
-      switchMap(_ => {
-        return postService.list(0, 100, this.username, this.year, this.month)
-      })
-    )
+    ).subscribe(([params, queryParams]) => {
+      this.username = params.get('username') ?? undefined
+      this.year = queryParams.get('year') ?? undefined
+      this.month = queryParams.get('month') ?? undefined
+      this.posts.clear()
+      this.getNextPage()
+    })
     this.selectedPost$ = this.route.queryParamMap.pipe(
       switchMap(queryParams => {
         let selectedShortcode = queryParams.get('selected')
-        if (selectedShortcode) {
-          let cachedPost = this.postService.getCached(selectedShortcode)
-          if (cachedPost) {
-            return new BehaviorSubject<Post>(cachedPost)
-          } else {
-            return this.postService.getPost(selectedShortcode)
-          }
+        let post = this.posts.get(selectedShortcode ?? '')
+        if (post) {
+          return new BehaviorSubject<Post>(post)
+        } else if (selectedShortcode) {
+          return this.postService.getPost(selectedShortcode)
         } else {
           return new BehaviorSubject(null)
         }
@@ -62,7 +58,11 @@ export class PostsGridComponent {
     )
   }
 
-  onScroll() {
-    console.log('scrolled!!');
+  getNextPage() {
+    this.postService.list(this.posts.size, 100, this.username, this.year, this.month).subscribe(response => {
+      response.posts.forEach(post => {
+        this.posts.set(post.shortcode, post)
+      })
+    })
   }
 }
