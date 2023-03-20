3fdf3502c7b86bdbd1b057f308677639d17388ac
Chris Li <chris@kiwix.org>
2023-03-19 21:33:16 -0400

    postgrid loading logic

--- web/src/app/posts-grid/posts-grid.component.ts
+++ web/src/app/posts-grid/posts-grid.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { filter, combineLatestWith, switchMap, map } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { combineLatestWith, distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 
 import { PostService, Post } from '../services/post.service';
 
@@ -14,9 +14,9 @@ import { PostService, Post } from '../services/post.service';
 export class PostsGridComponent {
   postService: PostService;
 
-  username?: string
-  year?: string
-  month?: string
+  private username?: string
+  private year?: string
+  private month?: string
   selectedPost$: Observable<Post | null>
   selectedUsername$: Observable<string | null>
 
@@ -24,18 +24,24 @@ export class PostsGridComponent {
     this.postService = postService
     this.route.paramMap.pipe(
       combineLatestWith(this.route.queryParamMap),
-      filter(([params, queryParams]) => {
+      map(([params, queryParams]) => {
+        return {
+          username: params.get('username'),
+          year: queryParams.get('year'),
+          month: queryParams.get('month'),
+        }
+      }),
+      distinctUntilChanged((previous, current) => {
         return (
-          this.username == undefined || 
-          params.get('username') != this.username || 
-          queryParams.get('year') != this.year ||
-          queryParams.get('month') != this.month
+          previous.username == current.username && 
+          previous.year == current.year && 
+          previous.month == current.month
         )
       }),
-    ).subscribe(([params, queryParams]) => {
-      this.username = params.get('username') ?? undefined
-      this.year = queryParams.get('year') ?? undefined
-      this.month = queryParams.get('month') ?? undefined
+    ).subscribe((data) => {
+      this.username = data.username ?? undefined
+      this.year = data.year ?? undefined
+      this.month = data.month ?? undefined
       this.postService.shortcodes = []
       this.postService.posts.clear()
       this.getNextPage()
